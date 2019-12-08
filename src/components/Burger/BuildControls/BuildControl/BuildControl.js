428574c9c4de3e01f62851aee71157f91bd2d8e5
Harrish Suhumar <harrish.s@hotmail.ca>
2019-12-08 15:55:58 -0500

    can add items to burger

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,9 @@ const buildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Remove</button>
-    <button className={classes.More}>Add</button>
+    <button className={classes.More} onClick={props.added}>
+      Add
+    </button>
   </div>
 );
 

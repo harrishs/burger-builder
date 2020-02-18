dbed23ae4288d7ea86dd4e6db4175040ffe8e7bf
Harrish Suhumar <harrish.s@hotmail.ca>
2020-02-17 21:40:28 -0500

    remove user token after 1hr for session expiry

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = error => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
@@ -44,6 +58,7 @@ export const auth = (email, password, isSignup) => {
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         console.log(err);

422876124f779d1751813325a33ad765d946ab99
Harrish Suhumar <harrish.s@hotmail.ca>
2020-03-15 18:38:23 -0400

    test container excluding redux

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,7 +23,7 @@ describe("<NavigationItems />", () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 
-  it("should render 3 navigation items elements if authenticated", () => {
+  it("should show logout button when logged in", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(
       wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
